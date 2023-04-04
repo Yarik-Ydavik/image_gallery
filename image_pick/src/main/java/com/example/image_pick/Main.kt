6c8c65d5d59be5586945d8acbb0c634f34b9cea5
Yaroslav Zagumennikov <yzagumennikov1@mail.ru>
2023-04-04 10:02:18 +0500

    Initial commit

---
+++ image_pick/src/main/java/com/example/image_pick/Main.kt
@@ -0,0 +1,13 @@
+package com.example.image_pick
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+class Main {
+    fun Click(context: Context){
+        Toast.makeText(context,"Hi",Toast.LENGTH_SHORT)
+    }
+
+}
